e083b7998bf516011a2893156bc2c0dc9a1b75d7
guilian.ganster@epitech.eu <guilian.ganster@epitech.eu@epitech.eu>
2019-02-11 15:47:25 +0100

    solve huge bug with Makefile link

--- include/my_ini.h
+++ include/my_ini.h
@@ -10,19 +10,24 @@
 
 #include "my_file.h"
 #include "my_str.h"
+#include "my_vector.h"
 
+//double vector_value contain char*
 struct ini_line {
     char *key;
-    char **value;
+    double_vector_t *value;
 };
 typedef struct ini_line ini_line_t;
 
+//double vector_line contain ini_line*
 struct ini_file {
-    ini_line_t *lines;
+    double_vector_t *lines;
     int size;
 };
 typedef struct ini_file ini_file_t;
 
 //ini line gestion
+ini_line_t *ini_line_create(char const *str);
+void ini_line_destroy(ini_line_t *entity);
 
 #endif /* !MY_INI_H_ */
