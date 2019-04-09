a0efc4e5e03d6deee53f7eba4057a007dfc4c446
guilian.ganster@epitech.eu <guilian.ganster@epitech.eu@epitech.eu>
2019-04-09 03:35:22 +0200

    line memory gestion

--- include/my_ini.h
+++ include/my_ini.h
@@ -11,16 +11,28 @@
 #include "my_str.h"
 #include "my_file.h"
 #include "my_vector.h"
+#include <stdio.h>
 
 struct ini_line {
     char *key;
-    double_vector_t *lines;
+    double_vector_t *values;
 };
 typedef struct ini_line ini_line_t;
 
+struct ini_file {
+    char *path;
+    double_vector_t *lines;
+};
+typedef struct ini_file ini_file_t;
+
+// ini_line.c
 ini_line_t *ini_line_create(char *key);
 ini_line_t *ini_line_create_from_str(char *str);
 void ini_line_destroy(ini_line_t *line);
 
-void ini_line_to_str(ini_line_t *line);
+// ini_line_conversion.c
+char *ini_line_to_str(ini_line_t *line);
+
+// ini_file.c
+
 #endif /* !MY_INI_H_ */
